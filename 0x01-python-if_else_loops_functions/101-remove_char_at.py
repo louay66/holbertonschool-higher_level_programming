f1a2ea43190b2cb3e0deabfb2d360fde56d77d08
louay66 <louaylouay20@gmail.com>
2022-04-26 14:47:56 -0700

    without thins index

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    new = ''
-    a = 0
-    for i in str:
-        if i != n:
-            new += str[i]
-        a += 1
+    new = str
+    if n >= 0:
+        new = new[:n] + new[n+1:]
     return new
