a29bd6b1db9788a3efa6fc5eaccefe72dedf356a
louay66 <louaylouay20@gmail.com>
2022-04-28 04:38:39 -0700

    infinte add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    i  = 0  
+    i = 0
     if len(argv) > 2:
         for c in range(1, len(argv)):
             i += int(argv[c])
 
     print(f"{i}")
-
