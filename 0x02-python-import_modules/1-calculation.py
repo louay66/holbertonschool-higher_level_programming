e27803c92d208ab3100478a4958fc2f72e0e0eeb
louay66 <louaylouay20@gmail.com>
2022-04-28 02:29:28 -0700

    import

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+print("10 + 5 = {}".format(add(a, b)))
+print("10 - 5 = {}".format(sub(a, b)))
+print("10 * 5 = {}".format(mul(a, b)))
+print("10 / 5 = {}".format(div(a, b)))
