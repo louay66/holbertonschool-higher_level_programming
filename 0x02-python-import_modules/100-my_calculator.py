86d88f681d4192ccce2b17e575ceccc703c3981c
louay66 <louaylouay20@gmail.com>
2022-04-28 14:39:33 -0700

    caluc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
-    if len(argv) < 2 or len(argv) > 4:
+    if len(argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
