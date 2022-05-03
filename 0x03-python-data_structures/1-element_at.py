10f540ef7eb2d607cd0e391be762656c435a1529
louay66 <louaylouay20@gmail.com>
2022-05-03 03:59:17 -0700

    elemenrt at

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    i = len(my_list) - 1
+    if idx < 0:
+        return none
+    elif idx > i:
+        return none
+    else:
+        return my_list[idx]
