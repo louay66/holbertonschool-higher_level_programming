a96d0ce720b872a6086c7050d70c4d0b66216b89
louay66 <louaylouay20@gmail.com>
2022-05-04 10:20:01 -0700

    squar matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    if matrix:
+        for i in matrix[:]:
+            new_matrix.append(list(map(lambda n: n**2, i)))
+        return new_matrix
