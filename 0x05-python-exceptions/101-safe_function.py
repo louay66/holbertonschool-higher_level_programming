2d2be33d632984a14a06938a4d8d7501840bc2e3
louay66 <louaylouay20@gmail.com>
2022-05-16 14:50:56 -0700

    exption error

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    try:
+        result = fct(*args)
+        return result
+    except Exception as a:
+        print("Exception: {}".format(a), file=sys.stderr)
+        return None
