95c619638421dfac609b35b1b3c3647987ac3068
louay66 <louaylouay20@gmail.com>
2022-05-17 14:16:27 -0700

    position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""This is a square
+"""
+
+
+class Square:
+    """Square Class
+        """
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """area
+        return area of squear
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """my_print
+        return rint of squear
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int and type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value != 2):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
