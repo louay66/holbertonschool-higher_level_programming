2a72ea1a422ee5bacd43a7d703bb0ccde17c6bd0
louay66 <louaylouay20@gmail.com>
2022-05-17 14:29:13 -0700

    __##

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -24,7 +24,11 @@ class Square:
         if self.__size == 0:
             print()
         else:
+            for x in range(0, self.__position[1]):
+                print()
             for i in range(0, self.__size):
+                for t in range(0, self.__position[0]):
+                    print(" ", end="")
                 for j in range(0, self.__size):
                     print("#", end="")
                 print()
