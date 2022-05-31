e08f7edfe913b5fb0388ce94a34198b085c1bbcc
louay66 <louaylouay20@gmail.com>
2022-05-31 15:07:40 -0700

    i fixed

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -17,7 +17,7 @@ class Student:
 
     def to_json(self, attrs=None):
 
-        if attrs is not None or type(attrs) != list:
+        if attrs is None or type(attrs) != list:
             return self.__dict__
         else:
             temp = {}
