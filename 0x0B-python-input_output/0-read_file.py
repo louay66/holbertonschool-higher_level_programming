a90eaa27dacd1b9d856de4d1842f5e98f0aa1569
louay66 <louaylouay20@gmail.com>
2022-05-31 07:00:48 -0700

    file io

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that reads a text file (UTF8)
+and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """function that reads a text file"""
+
+    with open(filename, encoding="utf-8") as myfile:
+
+        print(myfile.read(), end='')
