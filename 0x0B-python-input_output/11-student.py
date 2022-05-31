21ef35c75c450127ce94516bd2b2a88b6aac2832
louay66 <louaylouay20@gmail.com>
2022-05-31 15:19:51 -0700

    alle

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""a class Student that defines a student by"""
+
+
+class Student:
+    """class student
+        Public attributes:
+            - first_name
+            - last_name
+            - age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+
+        if attrs is None or type(attrs) != list:
+            return self.__dict__
+        else:
+            temp = {}
+            for elem in attrs:
+                if type(elem) != str:
+                    return self.__dict__
+                if elem in self.__dict__.keys():
+                    temp[elem] = self.__dict__[elem]
+            return temp
+
+    def reload_from_json(self, json):
+
+        for item in json.keys():
+            self.__dict__[item] = json[item]
