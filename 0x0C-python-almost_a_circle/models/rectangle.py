51ee7d47530a2721db073f6d8f1f965fb7bb9d62
louay66 <louaylouay20@gmail.com>
2022-06-06 10:22:29 -0700

    jh

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
-"""Create a Rectangle class, inheriting from Base"""
-from . base import Base
+"""Module rectangle.
+Create a Rectangle class, inheriting from Base.
+"""
+import json
+from models.base import Base
 
 
 class Rectangle(Base):
-    """ class inhert from Base
+    """Class describing a rectangle.
+    Public instance methods:
+        - area()
+        - display()
+        - to_dictionary()
+        - update()
+    Inherits from Base.
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ Init method
-            Args:
-                width(int): width of rectangle
-                height(int): height of rectangle
-                x(int): x
-                y(int): y
-                id(int): id inherited from Base
+        """Initializes a Rectangle instance.
+        Args:
+            - __width: width
+            - __height: height
+            - __x: position
+            - __y: position
+            - id: id
         """
+
         self.width = width
         self.height = height
         self.x = x
