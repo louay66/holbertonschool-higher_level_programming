c7465de46c64f23095565e5628f6447710e5c7d7
louay66 <louaylouay20@gmail.com>
2022-06-06 12:11:21 -0700

    ghfg

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,7 +10,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """init method"""
 
-        self.size = size
         super().__init__(size, size, x, y, id)
 
     @property
