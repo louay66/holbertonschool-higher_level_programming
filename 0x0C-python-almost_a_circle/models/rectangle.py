96f885be81f100060c5c6e2da7206a978fb38aa1
louay66 <louaylouay20@gmail.com>
2022-06-06 11:07:42 -0700

    kl

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,87 +11,121 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """__init__ method
+        this method initializes on instance creation
+        """
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
-    def __str__(self):
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} -\
- {self.__width}/{self.__height}"
-
     @property
     def width(self):
         return self.__width
-
-    @width.setter
-    def width(self, value):
-        self.check_value_w_h(value, "width")
-        self.__width = value
 
     @property
     def height(self):
         return self.__height
 
-    @height.setter
-    def height(self, value):
-        self.check_value_w_h(value, "height")
-        self.__height = value
-
     @property
     def x(self):
         return self.__x
-
-    @x.setter
-    def x(self, value):
-        self.check_value_w_h(value, "x")
-        self.__x = value
 
     @property
     def y(self):
         return self.__y
 
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be => 0")
+        self.__x = value
+
     @y.setter
     def y(self, value):
-        self.check_value_w_h(value, "y")
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be => 0")
         self.__y = value
 
-    def check_value_w_h(self, value, name):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value < 0 and name in ('x', 'y'):
-            raise ValueError(f"{name} must be >= 0")
-        if value <= 0 and name in ("height", "width"):
-            raise ValueError(f"{name} must be > 0")
-
     def area(self):
-        return self.__height * self.__width
+        """area Method"""
+
+        return self.__width * self.__height
 
     def display(self):
-        for space in range(self.__y):
-            print("")
-        for i in range(self.__height):
-            for space in range(self.__x):
-                print(" ", end='')
-            for j in range(self.__width):
-                print("#", end='')
-            print("")
+        """display Method"""
+
+        for a in range(0, self.__y):
+            print()
+        for i in range(0, self.__height):
+            for e in range(0, self.__x):
+                print(" ", end="")
+            for c in range(0, self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """__str__ method"""
+
+        return "[Rectangle] ({}) {}/{} - {}/{}" .format(
+                self.id, self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
-        argc = len(args)
-        kwargc = len(kwargs)
-        my_list = ["id", "width", "height", "x", "y"]
-        if argc > 0:
-            if argc > len(my_list):
-                argc = len(my_list)
-            for i in range(argc):
-                setattr(self, my_list[i], args[i])
-        elif kwargc > 0:
+        """update method"""
+
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                if type(args[0]) is not int and args[0] is not None:
+                    raise TypeError("id must be an integer")
+
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+        else:
             for key, value in kwargs.items():
-                if key in my_list:
-                    setattr(self, key, value)
+                if key == "id":
+                    if type(value) is not int and value is not None:
+                        raise TypeError("id must be an integer")
+                    self.id = value
+                if key == "width":
+                    self.width = value
+                if key == "height":
+                    self.height = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
 
     def to_dictionary(self):
-        return {"id": self.id, "width": self.width, "height":
-                self.height, "x": self.x, "y": self.y}
+        """to_dictionary Method"""
+
+        my_dict = {'x': self.x, 'y': self.y, 'id': self.id,
+                   'height': self.height, 'width': self.width}
+        return my_dict
