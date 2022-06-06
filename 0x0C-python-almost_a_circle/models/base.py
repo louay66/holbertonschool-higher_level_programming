35528cf3c997926fcaa2328b5cfddd9553bb6f77
louay66 <louaylouay20@gmail.com>
2022-06-06 12:21:00 -0700

    jhj

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,8 +26,7 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        if type(json_string) is not str\
-                or json_string is None or json_string == []:
+        if json_string is None or json_string == []:
             return "[]"
         else:
             return json.loads(json_string)
