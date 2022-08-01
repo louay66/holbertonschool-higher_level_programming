e4bb2504cc701d368480bc320c043a19e5a148f4
louay66 <louaylouay20@gmail.com>
2022-08-01 20:13:52 +0100

    fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+if (isNaN(process.argv[2]) || process.argv[2] === null) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i = i + 1) {
