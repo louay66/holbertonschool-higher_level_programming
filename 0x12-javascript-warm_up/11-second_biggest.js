f65f12ea34293d64ac1c15d74007a7671ece2a2a
louay66 <louaylouay20@gmail.com>
2022-08-01 21:19:57 +0100

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,14 +2,25 @@
 
 function checkandsort (args) {
   let sort = [];
+  let bool = 1;
+
   for (let i = 2; i < args.length; i = i + 1) {
     if (isNaN(args[i])) {
       return (0);
+    } else if (args[i] > 0) {
+      bool += 1;
     }
     list.push(parseInt(args[i]));
   }
+
   sort = list.sort();
-  return sort[(sort.length - 2)];
+  if (bool === 1) {
+    return sort[1];
+  } else if (bool === 2) {
+    return sort[0];
+  } else {
+    return sort[(sort.length - 2)];
+  }
 }
 const list = [];
 if (process.argv.length <= 3) {
