0b05542cbbbe0970cbe51abe0a7716867085babe
louay66 <louaylouay20@gmail.com>
2022-08-06 18:53:47 +0100

    fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE %s\
-                ORDER BY id ASC", (argv[4]))
+                ORDER BY id ASC", (argv[4],))
     rows = cur.fetchall()
     for row in rows:
         if (row[1] == argv[4]):
