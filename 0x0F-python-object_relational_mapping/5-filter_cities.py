5056b3d1bbfcf46ff48fda29f5ece50c0a1fa750
louay66 <louaylouay20@gmail.com>
2022-08-06 18:54:17 +0100

    fix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT  cities.name FROM cities JOIN states ON\
            cities.states_id = states.id WHERE states.name LIKE %s\
-              ORDER BY cities.id ", (argv[4]))
+              ORDER BY cities.id ", (argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
