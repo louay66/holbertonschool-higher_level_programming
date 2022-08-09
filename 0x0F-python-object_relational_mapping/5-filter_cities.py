ef28bb12bf2af5bb972aded1d1e73dfbb8aa40ee
louay66 <louaylouay20@gmail.com>
2022-08-09 11:21:39 +0100

    fix in output

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,14 +7,20 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    cout = 0
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT  cities.name FROM cities JOIN states ON\
-           cities.state_id = states.id WHERE states.name LIKE %s\
-              ORDER BY cities.id ", (argv[4],))
+    cur.execute("SELECT cities.id, cities.name, states.name FROM\
+        cities LEFT JOIN states ON cities.state_id = states.id\
+            ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if (row[2] == argv[4]):
+            if (cout > 0):
+                print(', ', end="")
+            print(row[1], end="")
+            cout = cout + 1
+    print()
     cur.close()
     db.close()
