93e4a99f14c2f09973b21274716fa99dc9abe047
louay66 <louaylouay20@gmail.com>
2022-08-19 22:42:33 +0100

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" My GitHub with API"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    ama = requests.get("https://api.github.com/user",
+                     auth=(argv[1], argv[2]))
+    res = ama.json()
+    try:
+        print(res['id'])
+    except KeyError:
+        print(None)
