0d5b683d1f2352284c4c9e1c7f27bfddc6750b6c
louay66 <louaylouay20@gmail.com>
2022-08-19 20:12:17 +0100

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Status of api."""
+import requests
+
+ama = requests.get('https://intranet.hbtn.io/status')
+html = ama.text
+print("Body response:")
+print("\t- type:", type(html))
+print("\t- content:", html)
