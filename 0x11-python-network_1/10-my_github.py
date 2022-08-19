235c3a790c7d85fd99fc289ad2fb21ab3048b9b5
louay66 <louaylouay20@gmail.com>
2022-08-19 22:43:35 +0100

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,7 +5,7 @@ from sys import argv
 
 if __name__ == "__main__":
     ama = requests.get("https://api.github.com/user",
-                     auth=(argv[1], argv[2]))
+                       auth=(argv[1], argv[2]))
     res = ama.json()
     try:
         print(res['id'])
