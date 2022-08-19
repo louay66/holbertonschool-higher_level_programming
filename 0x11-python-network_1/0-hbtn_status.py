635b6f57e5c124911b7ed81e3488f4524d55e4af
louay66 <louaylouay20@gmail.com>
2022-08-19 17:58:39 +0100

    fix in betty

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,9 @@
 """ fetches URL"""
 import urllib.request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
-        print(f"\t- type: {type(html)}")
-        print(f"\t- content: {html}")
-        print(f"\t- utf8 content: {html.decode('utf-8')}")
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print("Body response:")
+    print("\t- type:", type(html))
+    print("\t- content:", html)
+    print("\t- utf8 content:", html.decode('utf-8'))
